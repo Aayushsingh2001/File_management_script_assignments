dfac91581099b068efebdab4c016b164ffe04c1f
Aayush Singh <140009530+Aayushsingh2001@users.noreply.github.com>
2024-09-30 21:20:09 +0530

    backup_script.sh

--- task_1/backup_script.sh
+++ task_1/backup_script.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Create a backup directory in the user's home directory
+# Current directory where .txt files are stored.
 current_directory="/c/Users/as699/onedrive/desktop/aayu_devops/assignment_file_management_script/task_1/current_directory"
 
-# Create the directory if it doesn't exist
+# Backup directory where after the backup .txt file store.
 backup="/c/Users/as699/onedrive/desktop/aayu_devops/assignment_file_management_script/task_1/backup"
 
 # Get the current date and time in a specific format
 current_datetime=$(date +"%Y0-%m-%d >> %H:%M:%S")
 
+#make a directory when directory is not exist.
 mkdir -p "$backup"
 
-# Copy .txt files
+# Copy .txt files to current directory to backup
 cp "$current_directory"/*.txt "$backup"
 
+# Printing the current location, backup location and date & time 
 echo "current file directory is: $current_directry"
 echo "backup completed.....!"
 echo "after the backup file location is: $backup"
